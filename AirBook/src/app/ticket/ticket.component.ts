c56e58cd79913beb0fb57beca5021d0e6fe60fa9
jhon <jhonhedo@gmail.com>
2023-10-27 17:31:29 +0530

    27-10-23

---
+++ AirBook/src/app/ticket/ticket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css']
+})
+export class TicketComponent {
+
+}
