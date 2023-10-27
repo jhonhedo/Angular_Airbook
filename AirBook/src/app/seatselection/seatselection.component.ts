8a109c088842abd31fd8857a390adf12177349e7
Anjali <anjaligavre@gmail.com>
2023-10-27 17:01:41 +0530

    27-10-23

---
+++ AirBook/src/app/seatselection/seatselection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seatselection',
+  templateUrl: './seatselection.component.html',
+  styleUrls: ['./seatselection.component.css']
+})
+export class SeatselectionComponent {
+
+}
