65c7483cc3e92869d4e20ed5f6a4d7303d2eee19
jhon <jhonhedo@gmail.com>
2023-11-07 17:54:45 +0530

    17:54

--- AirBook/src/app/showflight/showflight.component.ts
+++ AirBook/src/app/showflight/showflight.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class ShowflightComponent {
 
+  flights: any[] = JSON.parse(sessionStorage.getItem('flights') || '[]');
+  
+  ngOnInit() {
+    
+    // "airline": null,
+    // "departureTime": "2023-11-05T10:00:00",
+    // "arrivalTime": "2023-11-05T15:30:00",
+    // "from": "Mumbai",
+    // "to": "Goa",
+      }
+
 }
+// <div *ngFor="let flight of flights">
+//   <div>
+//     <p>From: {{ flight.from }}</p>
+//     <p>To: {{ flight.to }}</p>
+//     <!-- Add more properties as needed -->
+//   </div>
+// </div>
+// export class FlightComponent implements OnInit {
+//   flights: any[] = JSON.parse(sessionStorage.getItem('flights') || '[]');
+
+//   constructor() { }
+
+//   ngOnInit() {
+//     // Load your flight data here or in the constructor
+//   }
+// }
