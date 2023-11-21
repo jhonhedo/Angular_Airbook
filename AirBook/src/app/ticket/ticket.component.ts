e6869e2c8c37540e83495bd5f60559aed340de64
Anjali <anjaligavre@gmail.com>
2023-11-21 19:15:12 +0530

    ticketfile

--- AirBook/src/app/ticket/ticket.component.ts
+++ AirBook/src/app/ticket/ticket.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.css']
 })
-export class TicketComponent {
+export class TicketComponent implements OnInit{
+  tickets: any[] = [];
+  constructor(private http: HttpClient) {}
 
+ 
+ ngOnInit(): void {
+  this.http.get<any[]>('http://localhost:8080/api/admin/users')
+    .subscribe(
+      data => {
+        this.tickets = data;
+        console.log(data);
+    })
+  }
+  }
+ 
+export class Ticket{
+  AirlineName!:String;
+  From:String;
+  To:String;
+  Passenger:String;
+  BoardTime:number;
+  Departure:number;
+  Arrival:number;
+  Seat:number;
 }
+
+
